7b0d0e0c635bc4cfea3f85d70b741d85b159923d
Arnaud <arnaud.becart@gmail.com>
2018-11-12 21:48:17 +0100

    third commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
